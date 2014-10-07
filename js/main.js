9c782cddcc85780af9205d58a25d5ab6a463236e
Marcelo Galvao <marcelogallvao@gmail.com>
2014-10-07 18:23:41 -0300

    banner topo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,50 @@
 $( function(){
 
+
+	/* banner link circular */
+	$('.c-link-circulo a').click(function(event) {
+		event.preventDefault();
+
+		clearInterval(intervalo);
+
+		// botao
+		$(this).parent().parent().find('a').removeClass('anima');
+		$(this).addClass('anima');
+
+
+		// animacao do banner
+		var ind = $(this).parent().index();
+		var li = $('#banner .c-img ul li');
+		var indAtual = $('#banner .c-img ul li.ativo').index();
+
+		if (ind !== indAtual){
+
+			// var li = $('#banner .c-img ul li');
+			if (ind > indAtual){
+				$(li).css('z-index', '1').removeClass('ativo');
+				li.eq(ind).css({'left': '100%',	'z-index': '2'}).addClass('ativo').show(0).animate({ left: 0}, 800, "easeInOutQuart");
+				li.eq(indAtual).animate({ left: '-100%'}, 800, "easeInOutQuart");
+			
+			}else if (ind < indAtual){
+				$(li).css('z-index', '1').removeClass('ativo');
+				li.eq(ind).css({'left': '-100%', 'z-index': '2'}).addClass('ativo').show(0).animate({ left: 0}, 800, "easeInOutQuart");
+				li.eq(indAtual).animate({ left: '100%'}, 800, "easeInOutQuart");
+			}
+		}
+	});
+
+	// passando automaticamente
+    // function iniIntervalo(){
+        var intervalo = setInterval( function(){
+            
+            var indAtual = $('#banner .c-img ul li.ativo').index();
+            if (indAtual < 2)
+            	$('#banner .c-link ul li').eq(indAtual+1).find('a').trigger('click');
+
+        }, 4000);
+    // }
+
+
 	// scrool nas etapas
 	var scrollingScreen = false;
 	$(".slide").mousewheel(function (event, delta) {
